59d076dbac800ecf4a4f067a393cc628051fb7d1
Gianni Mastroianni <mastroiannig@classes.local>
2026-05-15 15:58:59 +0200

    modif css

--- galerie.php
+++ galerie.php
@@ -19,3 +19,27 @@
         <a href="categories.php" class="btn btn-primary">Voir plus</a>
     </div>
 </div>
+
+
+
+
+
+
+<nav class="navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">BI2 - Portfolio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Categories">Mes compétences</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
